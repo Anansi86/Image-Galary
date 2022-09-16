2236402d1e07d637fa49dfe16ee73214063c6f9f
Anansi86 <shawn.staton@student.awesomeinc.org>
2022-09-16 18:02:47 +0000

    imgArray

---
+++ main.js
@@ -0,0 +1,21 @@
+//Declare a const array listing the filenames of each image, such as 'pic1.jpg'.
+//Declare a const object listing the alternative text for each image.
+//Loop through the array of filenames, and for each one, insert an <img> element inside the thumb-bar <div> that embeds that image in the page along with its alternative text.
+//Add a click event listener to each <img> inside the thumb-bar <div> so that, when they are clicked, the corresponding image and alternative text are displayed in the displayed-img <img> element.
+//Add a click event listener to the <button> so that when it is clicked, a darken effect is applied to the full-size image. When it is clicked again, the darken effect is removed again.
+
+const image = ["images/pic1.jpg", "images/pic2.jpg", "images/pic3.jpg", "images/pic4.jpg", "images/pic5.jpg"]
+
+
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+
+
+const newImage = document.createElement('img');
+newImage.setAttribute('src', xxx);
+newImage.setAttribute('alt', xxx);
+thumbBar.appendChild(newImage);
